3b41892909fdcaf7e087091f304799c9e18010b2
Lidia <lidia@estudante.ufscar.br>
2023-11-23 16:12:26 +0100

    share messages in ros2

--- octomap_ros2/run.sh
+++ octomap_ros2/run.sh
@@ -1,13 +1,17 @@
 xhost +local:docker
 docker run --privileged -it --rm \
+           -e ROS_MASTER_URI=http://$(ip route | awk '/^default via/ {print $5}' | xargs -I {} ip addr show {} | awk '/inet / {split($2, a, "/"); print a[1]}'):11311 \
            -e NVIDIA_DRIVER_CAPABILITIES=all \
            -e NVIDIA_VISIBLE_DEVICES=all \
+           -e ROS_DOMAIN_ID=17 \
            --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
-           --net=host \
+           -p 11311:11311 \
            --ipc=host \
-            -v /home/lidia/lrs2_ws/src/path_speed_flying/utils/subs_pc_mqtt_ros2.py:/root/ros2_ws/src/subs_pc_mqtt_ros2.py \
-            -v /home/lidia/lrs2_ws/src/path_speed_flying/utils/tf_broadcaster_unity.py:/root/ros2_ws/src/tf_broadcaster_unity.py \
+           --name my_ros2_container \
+           -v /home/lidia/lrs2_ws/src/path_speed_flying/utils/subs_pc_mqtt_ros2.py:/root/ros2_ws/src/subs_pc_mqtt_ros2.py \
+           -v /home/lidia/lrs2_ws/src/path_speed_flying/utils/tf_broadcaster_unity.py:/root/ros2_ws/src/tf_broadcaster_unity.py \
            --shm-size=4gb \
            --env="DISPLAY=$DISPLAY" \
            octomap-ros2:latest /bin/bash
-           
+
+#    --net=host \
--- octomap_ros2/run_octomap.sh
+++ octomap_ros2/run_octomap.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# tmux new-session -d -s my_session -n "octomap"
 tmux new-session -d -s my_session -n "tf"
 tmux new-window -t my_session:1 -n "pc_unity"
 tmux new-window -t my_session:2 -n "octomap"
+tmux new-window -t my_session:3 -n "test"
+
+# tmux send-keys -t my_session:0 "ros2 launch octomap_server octomap_mapping.launch.xml" Enter
 
 tmux send-keys -t my_session:0 "python3 ~/ros2_ws/src/tf_broadcaster_unity.py" Enter
 sleep 1
 tmux send-keys -t my_session:1 "python3 ~/ros2_ws/src/subs_pc_mqtt_ros2.py" Enter
 sleep 1
 tmux send-keys -t my_session:2 "ros2 launch octomap_server octomap_mapping.launch.xml" Enter
+sleep 1
+tmux send-keys -t my_session:3 "ros2 topic echo /occupied_cells_vis_array" Enter
 
 sleep 3
 
